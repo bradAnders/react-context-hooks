251cfb4bdfd4259fbde9a885c1171322e7a2234e
Brad Anderson <btanders@pm.me>
2020-07-21 12:26:25 -0700

    two contexts to change display

--- contextapp/src/components/Navbar.js
+++ contextapp/src/components/Navbar.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import { AuthContext } from '../contexts/AuthContext';
 
 class Navbar extends Component {
   render() {
     return (
-      <ThemeContext.Consumer>{({ isLightTheme, light, dark }) => {
-        const theme = isLightTheme ? light : dark;
-        return (
-          <nav style={ { background: theme.ui, color: theme.syntax } }>
-            <h1>Context App</h1>
-            <ul>
-              <li>Home</li>
-              <li>About</li>
-              <li>Contact</li>
-            </ul>
-          </nav>
-        )
-      }}</ThemeContext.Consumer>
+      <AuthContext.Consumer>{ ({ isAuthenticated, toggleAuth }) => (
+        <ThemeContext.Consumer>{ ({ isLightTheme, light, dark }) => {
+          const theme = isLightTheme ? light : dark;
+          return (
+            <nav style={ { background: theme.ui, color: theme.syntax } }>
+              <h1>Context App</h1>
+              <div onClick={ toggleAuth }>
+                { isAuthenticated ? 'Logged in' : 'Logged out' }
+              </div>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+          )
+        }}</ThemeContext.Consumer>
+      )}</AuthContext.Consumer>
     );
   }
 }
