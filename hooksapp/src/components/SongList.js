5a236caf40429e9a78d09d9921f4a7c4fb26aff8
Brad Anderson <btanders@pm.me>
2020-07-21 13:29:02 -0700

    Examples for conditional hooks

--- hooksapp/src/components/SongList.js
+++ hooksapp/src/components/SongList.js
@@ -1,16 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import uuid from 'uuid/dist/v1';
 import NewSongForm from './NewSongForm';
 
 const SongList = () => {
   const [songs, setSongs] = useState([
-    { title: 'almost home', id: 1 },
-    { title: 'memory gospel', id: 2 },
-    { title: 'this wild darkness', id: 3 },
+    { title: 'almost home', id: "e91e4b60-cb8f-11ea-b814-77cf182f02e8" },
+    { title: 'memory gospel', id: "ec7e1830-cb8f-11ea-b814-77cf182f02e8" },
+    { title: 'this wild darkness', id: "0fac9840-cb90-11ea-b814-77cf182f02e8" },
   ]);
+  const [age, setAge] = useState(20);
   const addSong = title => {
     setSongs([...songs, {title, id: uuid() }])
   };
+  useEffect(() => {
+    console.log('useEffect hook ran', songs);
+  }, [songs]);
+  useEffect(() => {
+    console.log('useEffect hook ran', age);
+  }, [age]);
   return (
     <div className="song-list">
       <ul>
@@ -19,6 +26,7 @@ const SongList = () => {
         }) }
       </ul>
       <NewSongForm addSong={ addSong } />
+      <button onClick={ () => setAge(age+1) }>Add 1 to age: { age }</button>
     </div>
   );
 }
